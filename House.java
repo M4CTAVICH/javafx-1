18e10834f1971021a466cf768726411726900acb
Ouaret Messaoud Chakib <120418528+M4CTAVICH@users.noreply.github.com>
2024-05-22 23:39:39 +0100

    Add files via upload

---
+++ House.java
@@ -0,0 +1,35 @@
+public class House {
+    private String type;
+    private double price;
+    private String address;
+
+    public House(String type, double price, String address) {
+        this.type = type;
+        this.price = price;
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
